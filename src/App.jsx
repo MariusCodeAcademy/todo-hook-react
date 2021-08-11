4c2bba8d0a04b3627be1900e298e21dc8548c560
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-11 11:58:28 +0300

    geting counter id and op value in handleCount in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,29 @@ function App() {
     { id: 4, value: 4 },
   ];
   const [counters, setCounters] = useState(counterData);
+
+  const handleCount = (id, operation) => {
+    console.log("counter with id", id, "operation", operation);
+
+    // const difference = special ? 2 : 1;
+    // const countFix = isNaN(count) ? 0 : count;
+
+    // if (operation === "-") return setCount(countFix - difference);
+    // if (operation === "+") return setCount(countFix + difference);
+    // if (operation === "rs")
+    //   return special ? setCount("start over") : setCount(0);
+  };
+
   return (
     <div className="App">
       <div className="d-flex">
         {counters.map((c) => (
-          <Counter item={c} key={c.id} special={c.isSpecial} />
+          <Counter
+            onHandleCount={handleCount}
+            item={c}
+            key={c.id}
+            special={c.isSpecial}
+          />
         ))}
       </div>
       <button>ResetAll</button>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Counter({ special, item }) {
-  const [count, setCount] = useState(0);
-  const handleCount = (operation) => {
-    const difference = special ? 2 : 1;
-    const countFix = isNaN(count) ? 0 : count;
-
-    if (operation === "-") return setCount(countFix - difference);
-    if (operation === "+") return setCount(countFix + difference);
-    if (operation === "rs")
-      return special ? setCount("start over") : setCount(0);
-  };
+// controlled component
+export default function Counter({ special, item, onHandleCount }) {
   const handleDisabled = () => {
-    if (special && count === 0) return true;
-    if (count === "start over") return true;
+    //   if (special && count === 0) return true;
+    //   if (count === "start over") return true;
+    return false;
   };
   return (
     <div style={{ width: "25%" }}>
       <h2>{special && "Special "}Counter</h2>
       <h2>{item.value}</h2>
       <div>
-        <button onClick={() => handleCount("+")}>{special && "2"}+</button>
-        <button disabled={handleDisabled()} onClick={() => handleCount("-")}>
+        <button onClick={() => onHandleCount(item.id, "+")}>
+          {special && "2"}+
+        </button>
+        <button
+          disabled={handleDisabled()}
+          onClick={() => onHandleCount(item.id, "-")}
+        >
           {special && "2"}-
         </button>
       </div>
-      <button onClick={() => handleCount("rs")}>reset</button>
+      <button onClick={() => onHandleCount(item.id, "rs")}>reset</button>
     </div>
   );
 }
