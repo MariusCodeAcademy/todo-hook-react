0a1c863418ac77ba016a4963c6b706a66fd29334
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-11 10:57:23 +0300

    special counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import Counter from "./components/Counter";
 
 function App() {
+  const counterData = [
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+  ];
+  const [counters, setCounters] = useState(counterData);
   return (
     <div className="App">
       <div className="d-flex">
         <Counter />
         <Counter />
-        <Counter />
+        <Counter special />
       </div>
       <button>ResetAll</button>
     </div>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 
-export default function Counter(props) {
+export default function Counter({ special }) {
+  //   const user = {
+  //     name: "mike",
+  //     age: 88,
+  //   };
+
+  //   const {name, age} = user // sukuria ta pati kas butu // const name = user.name
+
   const [count, setCount] = useState(0);
   const handleCount = (operation) => {
-    if (operation === "-") return setCount(count - 1);
-    if (operation === "+") return setCount(count + 1);
-    if (operation === "rs") return setCount(0);
+    const difference = special ? 2 : 1;
+    const countFix = isNaN(count) ? 0 : count;
+
+    if (operation === "-") return setCount(countFix - difference);
+    if (operation === "+") return setCount(countFix + difference);
+    if (operation === "rs")
+      return special ? setCount("start over") : setCount(0);
   };
   return (
-    <div>
-      <h2>Counter</h2>
+    <div style={{ width: "25%" }}>
+      <h2>{special && "Special "}Counter</h2>
       <h2>{count}</h2>
       <div>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => handleCount("-")}>-</button>
+        <button onClick={() => handleCount("+")}>{special && "2"}+</button>
+        <button onClick={() => handleCount("-")}>{special && "2"}-</button>
       </div>
       <button onClick={() => handleCount("rs")}>reset</button>
     </div>
